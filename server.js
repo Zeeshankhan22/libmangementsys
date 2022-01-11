c31dff4ca320cdda8d580efe6b7004ef5cb6f8a1
OSAMA-RIZWAN\Mor <osama@oddtechnologies.com>
2022-01-11 15:40:53 +0500

    Update V2

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const { application } = require("express");
 const express=require("express")
 const fs = require('fs');
+const bodyparser=require("body-parser")
 
 const app=express()
+
+app.use(bodyparser.urlencoded({extended:true}))
 
 app.use(express.static("Data"))
 
@@ -14,14 +17,50 @@ app.get("/",function(req,res){
 
 })
 app.post("/",function(req,res){
+
+
+var Fname=req.body.fullname
+var mail=req.body.email
+var password=req.body.pass
+
+if(Fname=="admin" && mail=="admin123@gmail.com" && password==123){
     res.sendFile(__dirname+"/admindis.html")
+
+    const myadmin={
+        Firstname:Fname,
+        Email:mail,
+        Password:password
+    
+    }
+    var jsondata=JSON.stringify(myadmin)
+    console.log(jsondata+"Real Admin Login")
+    }
+    else{
+        res.send("Wrong Admin")
+    }
+
+   
+ 
 })
 
+
+
+
+// const data={
+//            name:Fname,
+//            Email:mail,
+//            Password:password
+
+// }
+//var jsondata22=JSON.stringify(data)
+
+//console.log(jsondata22)
 
 
 app.get("/userlogin",function(req,res){
     res.sendFile(__dirname+("/userlogin.html"))
 })
+
 app.post("/userdash",function(req,res){
     res.sendFile(__dirname+"/userdash.html")
 })
@@ -33,8 +72,33 @@ app.get("/users",function(req,res){
     res.sendFile(__dirname+"/userlogin.html")
 
 })
-app.post("/signup",function(req,res){
+
+
+app.get("/signin",function(req,res){
     res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/signin",function(req,res){
+    res.sendFile(__dirname+"/userlogin.html")
+
+    var Fname=req.body.fname
+    var Lname=req.body.lname
+    var Uname=req.body.username
+    var mail=req.body.email2
+    var password=req.body.pass2
+    
+    
+    const data2={
+               Firstname:Fname,
+               Lastname:Lname,
+               Username:Uname,
+               Email:mail,
+               Password:password
+    
+    }
+    var jsondata2=JSON.stringify(data2)
+    console.log(jsondata2)
+
 })
 
 
